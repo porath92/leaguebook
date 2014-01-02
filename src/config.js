90b0d3590d4d4c55aa81bc495ba2c0df0d742f55
Clay Forbes <clay@madglor.com>
2014-01-02 14:00:55 -0500

    Added DB schema setup.
    Modified config to use leaguebook default database.

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 module.exports.configData = {
-  'dbURL' : process.env.HEROKU_POSTGRESQL_BLACK_URL || 'postgres://twitzer:twitzer@localhost:5432/twitzer'
+  'dbURL' : process.env.HEROKU_POSTGRESQL_BLACK_URL || 'postgres://leaguebook:leaguebook@localhost:5432/leaguebook'
 }
--- src/dbSetup.js
+++ src/dbSetup.js
@@ -1,38 +1,41 @@
-var configData = require('./config').configData;
 var pg         = require('pg');
+var configData = require('./config').configData;
 var psql       = new pg.Client(configData.dbURL);
-var waterfall  = require('async').waterfall;
 
-waterfall([
-  function (callback) {
-    psql.connect(function (err, res) {
-      console.log(err);
-      callback();
-    });
-  }, function (callback) {
-    psql.query('' +
-'DROP TABLE IF EXISTS account; ' +
-'' +
-'DROP SEQUENCE IF EXISTS account_id_seq; ' +
-'' +
-'CREATE SEQUENCE account_id_seq ' +
-'  INCREMENT BY 1' +
-'  MINVALUE 1 ' +
-'  NO MAXVALUE ' +
-'  START WITH 1; ' +
-'CREATE TABLE account ( ' +
-"  account_id numeric NOT NULL PRIMARY KEY DEFAULT nextval('account_id_seq')," +
-'  username   varchar(50) NOT NULL,' +
-'  followers  numeric NOT NULL DEFAULT 0,' +
-'  deaths     numeric NOT NULL DEFAULT 0,' +
-'  kills      numeric NOT NULL DEFAULT 0, ' +
-'  last_mentione_id numeric' +
-')' +
-    '', function (err, res) {
-      console.log(err);
-      callback();
-    });
-  }
-], function (err, res) {
+psql.connect();
+
+psql.query("\
+DROP TABLE IF EXISTS users; \
+DROP TABLE IF EXISTS college; \
+DROP TABLE IF EXISTS user_college_lnk; \
+DROP SEQUENCE IF EXISTS user_id_seq; \
+DROP SEQUENCE IF EXISTS college_id_seq; \
+\
+CREATE SEQUENCE user_id_seq \
+  INCREMENT BY 1 \
+  MINVALUE 1 \
+  NO MAXVALUE \
+  START WITH 1; \
+\
+CREATE TABLE users ( \
+  user_id    numeric NOT NULL PRIMARY KEY DEFAULT nextval('user_id_seq'), \
+  college_id numeric, \
+  email      varchar(50) NOT NULL \
+); \
+CREATE SEQUENCE college_id_seq \
+  INCREMENT BY 1 \
+  MINVALUE 1 \
+  NO MAXVALUE \
+  START WITH 1; \
+\
+CREATE TABLE college ( \
+  college_id numeric NOT NULL PRIMARY KEY DEFAULT nextval('college_id_seq'), \
+  name       varchar NOT NULL, \
+  size       varchar(20) NOT NULL, \
+  unitid     numeric(10) NOT NULL \
+); \
+\
+", function (res) {
+  console.log(res);
   psql.end();
 });
