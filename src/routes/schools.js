81e5e8f93d29fbdc0373ad7841f682699fe33248
Blain Smith <rebelgeek@blainsmith.com>
2014-01-02 15:56:44 -0500

    Reg form and school routes

--- src/routes/schools.js
+++ src/routes/schools.js
@@ -1,21 +1,46 @@
 module.exports = function(app) {
 
-  app.get('/schools/:school', function(req, res) {
-    
-    res.render('builder',
+  app.get('/schools', function(req, res) {
+
+    var schools = [
+      {
+        rank: 1,
+        name: 'College of Saint Rose',
+        slug: 'college-of-saint-rose',
+        summoner_count: 2
+      },
+      {
+        rank: 2,
+        name: 'Harvard University',
+        slug: 'harvard-university',
+        summoner_count: 1
+      }
+    ];
+
+    res.render('schools-index',
     {
-      layout: false,
-      tournament: JSON.stringify(tournament)
+      schools: schools
     });
-    
+
   });
 
   app.get('/schools/:school', function(req, res) {
-    
-    res.render('builder',
+
+    var summoners = [
+      {
+        rank: 1,
+        summoner: 'Shockem'
+      },
+      {
+        rank: 2,
+        summoner: 'ClayBot9000'
+      }
+    ];
+
+    res.render('schools-show',
     {
-      layout: false,
-      tournament: JSON.stringify(tournament)
+      school: 'College of Saint Rose',
+      summoners: summoners
     });
 
   });
