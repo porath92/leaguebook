418371a6fd2af3dcb30f4ca523e816f85e1c0443
Clay Forbes <clay@madglor.com>
2014-01-02 19:18:01 -0500

    Modified baseURL environment variable due to conflict with heroku environment variable.

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports.configData = {
-  'baseURL'   : process.env.HEROKU_URL                  || 'http://localhost:3000', 
+  'baseURL'   : process.env.BASE_URL                    || 'http://localhost:3000', 
   'dbURL'     : process.env.HEROKU_POSTGRESQL_BLUE_URL  || 'postgres://leaguebook:leaguebook@localhost:5432/leaguebook'
 }
