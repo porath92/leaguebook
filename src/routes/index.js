d61f24fed7a936f3e6a1a65835853bd74b282b1f
Blain Smith <rebelgeek@blainsmith.com>
2014-01-02 21:29:27 -0500

    Awesome random backgrounds FTW

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,19 @@ function routes(app) {
   var _ = require('underscore');
   
   app.get('/', function(req, res){
+
+    var champions = [
+      'Jinx',
+      'Ashe',
+      'Nocturne',
+      'Hecarim'
+    ];
+    var champion = champions[Math.floor(Math.random() * champions.length)];
     
     var registered = (req.query.r == 1) ? false : true;
     res.render('index',
     {
+      champion: champion,
       registered: registered
     });
     
