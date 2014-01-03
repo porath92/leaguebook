5fc2ee6c44142fc6a4a8110d17360087d4a25a7d
Blain Smith <rebelgeek@blainsmith.com>
2014-01-03 07:39:09 -0500

    Add Slug to response

--- src/routes/ajax.js
+++ src/routes/ajax.js
@@ -1,6 +1,6 @@
 module.exports = function(app) {
   app.get('/ajax/schools', function(req, res) {
-    app.psql.query(app.sql.select(['name', 'college_id'], 'college', "LOWER(name) LIKE LOWER('%" + req.query.name + "%')"), function (err, data) {
+    app.psql.query(app.sql.select(['name', 'college_id', 'slug'], 'college', "LOWER(name) LIKE LOWER('%" + req.query.name + "%')"), function (err, data) {
       res.writeHead(200, {'Content-Type': 'application/json'});
       res.write(JSON.stringify(data.rows));
       res.end();
