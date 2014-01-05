2669bd2d5e5fbe3266b6a1d11b5bb280581ee686
Blain Smith <rebelgeek@blainsmith.com>
2014-01-04 21:16:46 -0500

    Player count on schools listing

--- src/routes/schools.js
+++ src/routes/schools.js
@@ -2,7 +2,7 @@ var _ = require('underscore');
 
 module.exports = function(app) {
   app.get('/schools', function(req, res) {
-    app.psql.query('SELECT DISTINCT college.name, college, slug FROM college INNER JOIN users ON college.college_id = users.college_id ORDER BY college.name', function (err, data) {
+    app.psql.query('SELECT DISTINCT college.name, college.slug, COUNT(users.user_id) AS summoner_count FROM college INNER JOIN users ON college.college_id = users.college_id GROUP BY college.name, college.slug ORDER BY college.name', function (err, data) {
       res.render('schools-list',
       {
         schools: data.rows
