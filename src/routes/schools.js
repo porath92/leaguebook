2ec5b6cf0a230cd327f35d7019b811855b9e6dfb
Blain Smith <rebelgeek@blainsmith.com>
2014-01-09 22:59:06 -0500

    Score all the things. Closes #7

--- src/routes/schools.js
+++ src/routes/schools.js
@@ -11,13 +11,7 @@ module.exports = function(app) {
   });
 
   app.get('/schools/:school', function(req, res) {
-    app.psql.query(app.sql.select(
-      ['name', 'college_id', 'size', 'state'],
-      'college',
-      { 
-        slug: req.params.school
-      }
-    ), function(err, data) {
+    app.psql.query("select college.college_id, college.name, college.slug, COUNT(users.user_id) AS summoner_count, round(avg(users.rank)*count(users.rank)) as college_score from college inner join users on college.college_id = users.college_id where college.slug = '" + req.params.school + "' group by college.college_id, college.name, college.slug order by college_score DESC, college.name", function(err, data) {
       var school = data.rows[0];
       if(!_.isEmpty(school)) {
         app.psql.query(app.sql.getUsers(school.college_id), function(err, result) {
