3de2adcf5f804d9d820dae09ff99521b64c98490
Clay Forbes <clay@madglor.com>
2014-01-28 22:47:12 -0500

    Added sql error handling.

--- src/routes/ajax.js
+++ src/routes/ajax.js
@@ -1,6 +1,6 @@
 module.exports = function(app) {
   app.get('/ajax/schools', function(req, res) {
-    req.psql.query(req.sql.select(['name', 'college_id', 'slug'], 'college', "LOWER(name) LIKE LOWER('%" + req.query.name + "%')"), function (err, data) {
+    req.psql.psqlQuery(req.sql.select(['name', 'college_id', 'slug'], 'college', "LOWER(name) LIKE LOWER('%" + req.query.name + "%')"), function (err, data) {
       res.writeHead(200, {'Content-Type': 'application/json'});
       res.write(JSON.stringify(data.rows));
       res.end();
